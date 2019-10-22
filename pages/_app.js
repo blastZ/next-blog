84fb66bd0d2019530c0486e24905c5ef72ffd75b
blastz <touchzh1996@gmail.com>
2019-10-22 20:36:37 +0800

    build(css): edit css

--- pages/_app.js
+++ pages/_app.js
@@ -42,20 +42,90 @@ const Container = props => {
 const useStyles = makeStyles({
   '@global': {
     'html, body': {
-      // scrollBehavior: 'smooth',
-      overflowX: 'hidden',
-      margin: 0,
-      paddingRight: '0px !important'
+      margin: 0
+    },
+    html: {
+      boxSizing: 'border-box'
     },
     '*': {
+      boxSizing: 'inherit',
       fontFamily: `-apple-system,BlinkMacSystemFont,Helvetica Neue,Helvetica,Arial,PingFang SC,Hiragino Sans GB,Microsoft YaHei,sans-serif`
+    },
+    '*:before': {
+      boxSizing: 'inherit'
+    },
+    '*:after': {
+      boxSizing: 'inherit'
+    },
+    body: {
+      color: 'rgb(53,53,53)',
+      wordWrap: 'break-word',
+      fontKerning: 'normal',
+      fontFeatureSettings: "'kern', 'liga', 'clig', 'calt'"
+    },
+    a: {
+      color: '#ff5700',
+      textDecoration: 'none'
+    },
+    ul: {
+      margin: 0,
+      marginLeft: '1.65rem',
+      marginBottom: '1.32rem',
+      padding: 0,
+      listStylePosition: 'outside',
+      listStyleImage: 'none'
+    },
+    'ul li': {
+      paddingLeft: 0
+    },
+    li: {
+      marginBottom: 'calc(1.32rem / 2)'
+    },
+    'li > ul': {
+      marginLeft: '1.65rem',
+      marginBottom: 'calc(1.32rem / 2)',
+      marginTop: 'calc(1.32rem / 2)'
+    },
+    'li *:last-child': {
+      marginBottom: 0
+    },
+    'li > p': {
+      marginBottom: 'calc(1.32rem / 2)'
+    },
+    img: {
+      maxWidth: '100%'
+    },
+    p: {
+      margin: 0,
+      padding: 0,
+      marginBottom: 32
+    },
+    table: {
+      margin: 0,
+      marginBottom: '1.32rem',
+      fontSize: '1rem',
+      lineHeight: '1.65rem',
+      borderCollapse: 'collapse',
+      width: '100%'
+    },
+    thead: {
+      textAlign: 'left'
+    },
+    'td, th': {
+      textAlign: 'left',
+      borderBottom: '1px solid hsla(0, 0%, 0%, 0.12)',
+      fontFeatureSettings: 'tnum',
+      paddingLeft: '1.1rem',
+      paddingRight: '1.1rem',
+      paddingTop: '0.825rem',
+      paddingBottom: 'calc(0.825rem - 1px)'
+    },
+    'th:first-child, td:first-child': {
+      paddingLeft: 0
+    },
+    'th:last-child, td:last-child': {
+      paddingRight: 0
     }
-    // '@font-face': {
-    //   fontFamily: 'Ali-Regular',
-    //   src: `local('Alibaba-PuHuiTi-Regular'),
-    //     url('https://res.cloudinary.com/stackbunch/raw/upload/v1565955456/blog-assets/Alibaba-PuHuiTi-Regular_dp8dam.woff2')`,
-    //   fontDisplay: 'swap'
-    // }
   }
 });
 
